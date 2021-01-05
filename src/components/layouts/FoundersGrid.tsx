cd5f90c79a0ffb2fc6e0a843c257e7ea8f1024a5
Christine Kim <chrismekim@gmail.com>
2021-01-05 15:11:16 -0600

    Founder Info Block subheading

--- src/components/layouts/FoundersGrid.tsx
+++ src/components/layouts/FoundersGrid.tsx
@@ -13,6 +13,9 @@ type Props = {
 const FoundersGrid: React.FC<Props> = ({ founders, className }) => {
   return (
     <div className={cx('FoundersGrid text-module-padding-x', className)}>
+      <div className="pb2_25 md:pb2_25 col-12 md:col-10 mxauto color-charcoal primary-sm">
+        Founders
+      </div>
       <div className="FoundersGrid__grid-container col-12 md:col-10 mxauto">
         {founders.map((founder: Founder) => (
           <div key={founder.firstName} className="FoundersGrid__card-container">
